321c1f74a9ee44363b916a47e76e03548ae0ee74
Yaphet Kebede <kebedey@ad.renci.org>
2019-09-25 13:20:42 -0400

    onto service is a relations function

--- greent/services/onto.py
+++ greent/services/onto.py
@@ -1,6 +1,8 @@
 import json
 from greent.cachedservice import CachedService
 from greent.util import LoggingUtil
+from greent.graph_components import KNode, KEdge, LabeledID
+
 
 logger = LoggingUtil.init_logging(__name__)
 
@@ -62,22 +64,24 @@ class Onto(CachedService):
     def get_parents(self, identifier):
         return self.get(f"{self.url}/parents/{identifier}")['parents']
 
+    def get_children(self, identifier):
+        return self.get(f"{self.url}/children/{identifier}")
 
         
-    def onto_curie_to_parent_curie(self, node):
+    def get_ontological_subclass(self, node):
         #Ideally our ancestory list would be same as our query node
-
-        response = self.get_parents(node.id)
-        results = []
-        predicate = LabeledID(identifier="GAMMA:0000003", label="is_a")        
-        for curie in response:
-            name = self.get_label(curie)
-            new_node = KNode(curie, type = node.type, name = name)                        
-            results.append((
-                self.create_edge(
-                    node,
-                    new_node,
-                    'HPO.get_anscestors',
-                    node.id,
-                    predicate), new_node))
+        for parent_curie, lbl in node.synonyms:
+            response = self.get_children(parent_curie)
+            results = []
+            predicate = LabeledID(identifier="GAMMA:0000003", label="is_a")        
+            for curie in response:
+                name = self.get_label(curie)
+                new_node = KNode(curie, type = node.type, name = name)                        
+                results.append((
+                    self.create_edge(
+                        new_node,
+                        node,
+                        'onto.get_onthological_children',
+                        node.id,
+                        predicate), new_node))
         return results
