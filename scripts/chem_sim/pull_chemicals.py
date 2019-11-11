f013081edca99f94b16e7397870f7deb2358e7cc
Phil Owen <phillipsowen@outlook.com>
2019-11-11 12:01:14 -0500

    changing to use inchikeys instead

--- scripts/chem_sim/pull_chemicals.py
+++ scripts/chem_sim/pull_chemicals.py
@@ -15,13 +15,13 @@ def run_query(url,cypherquery):
 
 def get_chemicals(url):
     """This is all the variants.  We might want to filter on source"""
-    cquery = f'''match (a:chemical_substance) where a.smiles is not NULL RETURN a.id, a.name,a.smiles'''
+    cquery = f'''match (a:chemical_substance) where a.smiles is not NULL RETURN a.inchikey, a.name, a.smiles'''
     records = run_query(url,cquery)
     with open('smiles.txt','w') as outf:
         for r in records:
             print(r)
-            outf.write(f'{r["a.id"]}\t{r["a.name"]}\t{r["a.smiles"]}\n')
+            outf.write(f'{r["a.inchikey"]}\t{r["a.name"]}\t{r["a.smiles"]}\n')
 
 if __name__ == '__main__':
-    url = 'bolt://robokopdb.renci.org:7687'
+    url = 'bolt://robokopdb2.renci.org:7687'
     get_chemicals(url)
