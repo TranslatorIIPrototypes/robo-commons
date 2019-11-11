688726b3054a33062abdd103d8fc72da2d09be90
Phil Owen <phillipsowen@outlook.com>
2019-11-11 13:25:28 -0500

    making sure records with no inchi key

--- scripts/chem_sim/pull_chemicals.py
+++ scripts/chem_sim/pull_chemicals.py
@@ -15,7 +15,7 @@ def run_query(url,cypherquery):
 
 def get_chemicals(url):
     """This is all the variants.  We might want to filter on source"""
-    cquery = f'''match (a:chemical_substance) where a.smiles is not NULL RETURN a.inchikey, a.smiles'''
+    cquery = f'''match (a:chemical_substance) where a.smiles is not NULL and a.inchikey is not null RETURN a.inchikey, a.smiles'''
     records = run_query(url,cquery)
     with open('smiles.txt','w') as outf:
         for r in records:
