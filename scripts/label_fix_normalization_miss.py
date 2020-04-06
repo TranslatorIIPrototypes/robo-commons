69b18af234094b2fa34c649b046d7f15122eecb9
YaphetKG <kebedey@renci.org>
2020-04-06 11:52:35 -0400

    labels fix update

--- scripts/label_fix_normalization_miss.py
+++ scripts/label_fix_normalization_miss.py
@@ -5,7 +5,7 @@ class DBDriver:
     def __init__(self):
         neo4j_host = os.environ['NEO4J_HOST']
         neo4j_bolt = os.environ['NEO4J_BOLT_PORT']
-        neo4j_user = os.environ['NEO4J_USERNAME']
+        neo4j_user = 'neo4j'
         neo4j_password = os.environ['NEO4J_PASSWORD']
         self.driver = GraphDatabase.driver(
             uri=f'bolt://{neo4j_host}:{neo4j_bolt}',
@@ -24,15 +24,26 @@ def run_fix():
         'CARO': ':anatomical_entity:biological_entity:organismal_entity',
         'OMIM': ':biological_entity:disease:disease_or_phenotypic_feature',
         'DOID': ':biological_entity:disease:disease_or_phenotypic_feature',
-        'CP': ':anatomical_entity:cellular_component'
+        'CP': ':anatomical_entity:cellular_component',
+        'NCBIGene': ':gene:gene_or_gene_product:biological_entity:genomic_entity:macromolecular_machine:molecular_entity',
+        'UniProtKB': ':gene:gene_or_gene_product:biological_entity:genomic_entity:macromolecular_machine:molecular_entity',
+        'CHEMBL.COMPOUND': ':biological_entity:chemical_substance:molecular_entity',
+        'GTOPDB': ':biological_entity:chemical_substance:molecular_entity',
+        'HGNC': ':gene:gene_or_gene_product:biological_entity:genomic_entity:macromolecular_machine:molecular_entity',
+        'HMDB': ':biological_entity:chemical_substance:molecular_entity',
+        'PANTHER.FAMILY': ':gene_family',
+        'NCBITaxon': ':organism_taxon',
+        'NCBIGENE': ':gene:gene_or_gene_product:biological_entity:genomic_entity:macromolecular_machine:molecular_entity',
+        'UNIPROTKB': ':gene:gene_or_gene_product:biological_entity:genomic_entity:macromolecular_machine:molecular_entity'
     }
+
     make_query = lambda prefix, labels: f"MATCH (a) " \
         f"WHERE 1 = length(labels(a)) " \
         f"AND NOT a:Concept " \
         f"AND a.id STARTS WITH '{prefix}' " \
         f"SET a{labels} RETURN COUNT(a) as count"
 
-    for prefix, labels in fix_map:
+    for prefix, labels in fix_map.items():
         query = make_query(prefix, labels)
         print(f'running query \n {query}')
         count = db_driver.run(query).single()['count']
