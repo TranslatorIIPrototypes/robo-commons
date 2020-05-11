6989e530a7798dfaffd2bed7076569c454f391e3
Yaphetkg <kebedey@renci.org>
2020-05-11 10:59:22 -0400

    Async client retry operations

--- greent/annotators/util/async_client.py
+++ greent/annotators/util/async_client.py
@@ -1,40 +1,41 @@
 import aiohttp
 import logging
+from aiohttp_retry import RetryClient
 
 logger = logging.getLogger(name = __name__)
 
-async def async_get_json(url, headers = {}):
+async def async_get_json(url, headers = {},  retry_opts={'retry_attempts':3}):
     """
         Gets json response from url asyncronously.
     """
-    async with aiohttp.ClientSession() as session :
+    async with RetryClient() as session:
         try:
-            async with session.get(url, headers= headers) as response:
+            async with session.get(url, headers=headers, **retry_opts) as response:
                 if response.status != 200:
-                    logger.debug(f"Failed to get response from {url}. Status code {response.status}")
+                    logger.error(f"Failed to get response from {url}. Status code {response.status}")
                     return {}
                 return await response.json()
-        except:
-            logger.debug(f"Failed to get response from {url}." )
+        except Exception as e:
+            logger.error(f"Failed to get response from {url}. Failed with client opts {retry_opts}. Exception: {e}" )
             return {}
 
-async def async_get_text(url,headers = {}):
+async def async_get_text(url,headers = {},  retry_opts={'retry_attempts':3}):
     """
         Gets text response from url asyncronously
     """
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers= headers) as response:
+    async with RetryClient() as session:
+        async with session.get(url, headers= headers, **retry_opts) as response:
             if response.status != 200:
                 logger.error(f'Failed to get response from {url}, returned status : {response.status}')
                 return ''
             return await response.text()
 
-async def async_get_response(url, headers= {}):
+async def async_get_response(url, headers= {}, retry_opts={'retry_attempts':3}):
     """
     Returns the whole reponse object
     """
-    async with aiohttp.ClientSession() as session:
-        async with session.get(url, headers= headers) as response:
+    async with RetryClient() as session:
+        async with session.get(url, headers=headers, **retry_opts) as response:
             json = await response.json()
             text = await response.text()
             raw = await response.read()
