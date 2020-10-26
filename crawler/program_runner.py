65e006992add208b4aeaa13daa363de758a28a02
YaphetKG <kebedey@renci.org>
2020-10-26 12:35:24 +0000

    adding kegg check for when data returned from api is xml

--- crawler/program_runner.py
+++ crawler/program_runner.py
@@ -224,16 +224,25 @@ def get_identifiers(input_type,rosetta):
         # go for KEGG
         print('pull KEGG')
         content = requests.get('http://rest.kegg.jp/list/compound').content.decode('utf-8')
-    
+        line_counter = 0
         for line in content.split('\n'):
             if line :
+                contains_tab = '\t' in line 
+                if not contains_tab:
+                    print(f"expected tab but not found line : {line} ")
+                    print(f"error parsing line {line_counter}")
+                    with open('kegg-file.xml', 'w') as file:
+                        file.write(content)
+                    exit(1) 
+                
                 identifier, label = line.split('\t')
                 identifier = identifier.replace('cpd', 'KEGG')
                 identifier= identifier.replace('CPD', 'KEGG')
                 # maybe pick the first one for kegg,
                 label = label.split(';')[0].strip(' ')
                 lids.append(LabeledID(identifier, label))
-        
+                line_counter += 1
+                        
         for ident in identifiers:
             try:
                 lids.append(LabeledID(ident,chebi_labels[ident]))
