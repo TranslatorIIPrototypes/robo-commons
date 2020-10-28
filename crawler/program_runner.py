ee63cd7deaba4e07ab4981d8668e5c570332ebab
YaphetKG <kebedey@renci.org>
2020-10-28 12:25:41 -0400

    Kegg prefix updates

--- crawler/program_runner.py
+++ crawler/program_runner.py
@@ -255,7 +255,7 @@ def get_identifiers(input_type,rosetta):
         n=0
         for gtopdb_ligand in gtopdb_ligands:
             try:
-                lids.append(LabeledID(f"gtop:{gtopdb_ligand['ligandId']}",gtopdb_ligand['name']))
+                lids.append(LabeledID(f"gtpo:{gtopdb_ligand['ligandId']}",gtopdb_ligand['name']))
                 n+=1
             except:
                 print(gtopdb_ligand)
